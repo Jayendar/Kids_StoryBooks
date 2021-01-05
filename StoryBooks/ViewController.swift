f39097329cbfefbf5a69f7684c7dc4aa7b77c78c
Jayendar A <jayendar@Jayendars-MacBook-Air.local>
2021-01-05 15:26:01 +0530

    Initial Commit

---
+++ StoryBooks/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  StoryBooks
+//
+//  Created by Jayendar A on 04/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func buttonClicked(_ sender: Any) {
+        performSegue(withIdentifier: "MyCollection", sender: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
